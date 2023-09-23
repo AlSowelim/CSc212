8ee641793f51bc8ed709002edc2c4bd5135e0cdb
AlSowelim <mohammedalsowelim1@gmail.com>
2023-09-24 01:30:08 +0300

    Your commit message

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Contact {
+public class Contact implements Comparable<Contact> {
+    String name ,  email , address, bday ,notes;
+    int phoneNum;
+
+    public Contact(String name, String email, String address, String bday, String notes, int phoneNum)
+    {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.bday = bday;
+        this.notes = notes;
+        this.phoneNum = phoneNum;
+    }
+
+    @Override
+    public int compareTo(Contact o) {
+        if (this.name.compareTo(o.name)>0)
+        {
+            return 1;
+        }
+        else if (this.name.compareTo(o.name)<0){
+            return -1;
+        }
+        return 0;
+    }
 }
